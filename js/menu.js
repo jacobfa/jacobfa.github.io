5c25a332664031223c58908ac06b0e16b603b4f8
jacobfa <jacobfeinashley@gmail.com>
2026-03-06 20:21:04 -0800

    update

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+var validPages = ["home", "publications", "personal"];
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,56 +9,55 @@ function myFunction() {
   }
 }
 
-function openpage(evt, pagename, pushState) {
+function showPage(pagename) {
   var i, tabcontent, tablinks;
 
+  // Hide all tab content
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
+  // Remove active from all tab links
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  document.getElementById(pagename).style.display = "block";
+  // Show the selected tab
+  var el = document.getElementById(pagename);
+  if (el) el.style.display = "block";
 
   // Mark the matching nav link active
   for (i = 0; i < tablinks.length; i++) {
-    if (tablinks[i].getAttribute("onclick") &&
-        tablinks[i].getAttribute("onclick").indexOf("'" + pagename + "'") !== -1) {
+    var oc = tablinks[i].getAttribute("onclick") || "";
+    if (oc.indexOf("'" + pagename + "'") !== -1) {
       tablinks[i].className += " active";
-    }
-  }
-
-  // Update the URL hash (pushState = false when called from popstate/load)
-  if (pushState !== false) {
-    history.pushState({ page: pagename }, "", "#" + pagename);
-  }
-
-  // Close mobile menu if open
-  var scr = window.matchMedia("(max-width: 600px)");
-  if (scr.matches) {
-    var nav = document.getElementById("myTopnav");
-    if (nav.className !== "topnav") {
-      nav.className = "topnav";
     }
   }
 }
 
-// Handle back/forward browser navigation
+function openpage(evt, pagename) {
+  showPage(pagename);
+  history.pushState({ page: pagename }, "", "#" + pagename);
+
+  // Close mobile menu if open
+  var scr = window.matchMedia("(max-width: 600px)");
+  if (scr.matches) {
+    document.getElementById("myTopnav").className = "topnav";
+  }
+}
+
+// Back / forward button support
 window.addEventListener("popstate", function(e) {
   var page = (e.state && e.state.page) ? e.state.page : "home";
-  openpage(null, page, false);
+  showPage(page);
 });
 
-// On initial load, read the hash or default to home
+// On initial page load, honour the hash or default to home
 window.addEventListener("DOMContentLoaded", function() {
   var hash = window.location.hash.replace("#", "");
-  var validPages = ["home", "publications", "personal"];
   var page = validPages.indexOf(hash) !== -1 ? hash : "home";
-  // Replace current history entry so popstate works from the start
   history.replaceState({ page: page }, "", "#" + page);
-  openpage(null, page, false);
+  showPage(page);
 });
